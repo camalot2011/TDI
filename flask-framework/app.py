5ee8c97dfe1d1975e0019333010663e3187fd004
zhandi.liao@gmail.com <zhandi.liao@gmail.com>
2018-09-06 02:30:30 +0000

    Add data analysis

--- flask-framework/app.py
+++ flask-framework/app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template, request, redirect
+import requests
+import simplejson as json
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def index():
-  return render_template('index2.html')
+    if request.method == "GET":
+        return render_template('index2.html')
+    else:
+        symbol = request.form["symbol"]
+        start_date = request.form["start_date"]
+        end_date = request.form["end_date"]
+        serviceurl = 'https://www.quandl.com/api/v3/datasets/WIKI/'
+        symboljs = symbol+'.json'
+        serviceurl_symbol = serviceurl+symboljs
+        req = {'start_date':start_date,'end_date': end_date}
+        data = requests.get(serviceurl_symbol,params = req)
+        js = json.loads(data.content)
+        df = pd.DataFrame(js['dataset']['data'],columns = js['dataset']['column_names'])
+        df['Date'] = pd.to_datetime(df['Date'],format = "%Y-%M-%d").dt.date
+        df.set_index(['Date'],drop = True, inplace=True)
+        return render_template('about.html')
+
 
 @app.route('/about')
 def about():
